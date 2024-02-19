ac5806467456f05c5124ebbee0d28867109103e2
Jay Lee <pearlismylove@gmail.com>
2024-02-19 22:57:51 +0900

    Test ObjectMethods

--- src/main/java/com/pearlismylove/javaspringstudy/java/ObjectMethods.java
+++ src/main/java/com/pearlismylove/javaspringstudy/java/ObjectMethods.java
@@ -1,2 +1,16 @@
-package com.pearlismylove.javaspringstudy.java;public class ObjectMethods {
+package com.pearlismylove.javaspringstudy.java;
+
+import java.util.Objects;
+
+public class ObjectMethods {
+
+    public static void main(String[] args) {
+        String str = "Hello, World!";
+        String strByReference = str;
+        String strByValue = "Hello, World!";
+        System.out.println("str.hashCode() = " + str.hashCode());
+        System.out.println("strByReference.equals() = " + str.equals(strByReference));
+        System.out.println("strByValue.equals() = " + str.equals(strByValue));
+        System.out.println("Object.hash=" + Objects.hash(str));
+    }
 }
